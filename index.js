7f7c3da256ea8a2b602ee59bb35b1d3200c2881e
lpolverino <57161796+lpolverino@users.noreply.github.com>
2023-08-07 17:15:29 -0300

    added bbdd

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
+const cors = require('cors')
 const morgan = require('morgan')
-app.use( express.json())
+const Person = require('./models/Person')
+
+app.use(cors())
 app.use(express.static('build'))
+app.use( express.json())
+
 
 morgan.token('data',(request, response)=>{
   return JSON.stringify(request.body)
@@ -11,49 +17,49 @@ morgan.token('data',(request, response)=>{
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 
-let persons =
-[
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+const unknowEndPoint = (request,response) =>{
+  response.status(404).send({error: 'uknow endpoint'})
+}
+
+const errorHandler = (error, request, response, next) =>{
+  console.error(error.message);
+
+  if (error.name === 'CastError'){
+    return response.status(400).send({error:'malformated id'})
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+  next(error)
+}
+
+
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons =>{
+    response.json(persons)
+  })
+  
 })
 
 app.get('/info', (request, response) =>{
+  Person.find({}).then(persons =>{
     console.log(`Phonebook has info for ${persons.length} people`);
     response.send(
         `<p>Phonebook has info for ${persons.length} people </p>
         <p> ${new Date()}</p>
     `)
+  })
+ 
 })
 
 const generateId = () => {
-    return Math.floor(Math.random()*1000000000)
+    return Math.floor(Math.random()*10000)
   }
-
+/*
+ THIS IS WITHOUT MONGODB
 app.get('/api/persons/:id', (request,response) =>{
     const id = Number(request.params.id)
     const person = persons.find(person=> person.id === id)
@@ -85,7 +91,59 @@ app.post('/api/persons', (request, response) =>{
     }
     persons = persons.concat(person)
     response.json(person)
+})*/
+
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      response.json(person)
+     })
+    .catch(error =>next(error))
 })
+
+
+app.post('/api/persons', (request, response, next) =>{
+  const body = request.body
+  if(body.name === undefined){
+    return response.status(400).json({error:'content missing'})
+  }
+  const person = new Person({
+    name:body.name,
+    number:body.number,
+  })
+
+  person.save()
+  .then(savedPerson=>{
+    response.json(savedPerson)
+  })
+  .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next)=>{
+  Person.findByIdAndRemove(request.params.id)
+    .then(result=>{
+      response.status(204).end()
+    })
+    .catch(error =>next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next)=>{
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, {new:true})
+    .then(updatedPerson=>{
+      response.json(updatedPerson)
+    })
+    .catch(error=>next(error))
+})
+
+app.use(unknowEndPoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
