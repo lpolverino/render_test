4dc2e8ac4f898512aa88b20a6ddd34343dfc4c0c
lpolverino <57161796+lpolverino@users.noreply.github.com>
2023-08-07 17:44:53 -0300

    Update Person.js

--- models/Person.js
+++ models/Person.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 const url = process.env.MONGODB_URI
 console.log('connection to', url)
+console.log(typeof url);
 
 mongoose.connect(url)
     .then(result => {
